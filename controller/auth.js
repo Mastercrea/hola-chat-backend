db26f5d604810d60e1def54798fbb173feee5227
Mastercrea <mastercrea1928@gmail.com>
2022-06-13 16:16:59 -0400

    changes in authserver - user model

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,10 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
-const {varlidateGoogleIdToken} = require('../helpers/google-verify-token');
+const { varlidateGoogleIdToken } = require('../helpers/google-verify-token');
 const User = require('../models/user');
-const {generateJWT} = require("../helpers/jwt");
+const { generateJWT } = require("../helpers/jwt");
 
-const createUser = async (req, res = response) =>{
+const createUser = async (req, res = response) => {
     // extract specific data from body
     const { email, password } = req.body;
 
@@ -26,14 +26,14 @@ const createUser = async (req, res = response) =>{
         await user.save();
 
         // Generate JWT
-         const token = await generateJWT(user.id);
+        const token = await generateJWT(user.id);
 
         res.json({
             ok: true,
             user, token
         });
 
-    } catch (err){
+    } catch (err) {
         console.error(err);
         res.status(500).json({
             ok: false,
@@ -45,21 +45,21 @@ const createUser = async (req, res = response) =>{
 const login = async (req, res = response) => {
     // extract specific data from body
     const { email, password } = req.body;
-    try{
+    try {
         const userDB = await User.findOne({ email });
-        if(!userDB) {
+        if (!userDB) {
 
             return res.status(404).json({
-                ok:false,
+                ok: false,
                 msg: 'email not found'
             })
 
         }
         // validate password
         const validPassword = bcrypt.compareSync(password, userDB.password);
-        if(!validPassword){
+        if (!validPassword) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 msg: 'password not valid'
             })
         }
@@ -82,15 +82,15 @@ const login = async (req, res = response) => {
     }
 
 }
-const renewToken = async ( req, res = response) => {
+const renewToken = async (req, res = response) => {
 
-const uid = req.uid;
-const token = await generateJWT(uid);
-const user = await User.findById(uid);
+    const uid = req.uid;
+    const token = await generateJWT(uid);
+    const user = await User.findById(uid);
 
 
     res.json({
-        ok:true,
+        ok: true,
         user,
         token
     });
@@ -98,29 +98,48 @@ const user = await User.findById(uid);
 }
 
 const googleAuth = async (req, res = response) => {
-    console.log('googleAuth');
-    const token = req.body.token;
-    const googleUser = await varlidateGoogleIdToken(token);
+    try {
+        const token = req.body.token;
+        const {name, picture, email} = await varlidateGoogleIdToken(token);
+        let user = await User.findOne({email});
+        if (!user) {
 
-    if (!googleUser) {
+            const data = {
+                name,
+                email,
+                password: '-',
+                picture,
+                google: true,
+            };
 
-        return res.status(400).json({
-            ok: false
-        })
+            user = new User(data);
+            await user.save();
 
+            return res.status(400).json({
+                ok: false
+            })
+
+        }
+
+        if (!token) {
+            return res.json({
+                ok: false,
+                msg: 'Token not found'
+            })
+        }
+        res.json({
+            ok: true,
+            user
+        });
+    } catch (error) {
+
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token not valid'
+        });
     }
 
-    if (!token) {
-        return res.json({
-            ok: false,
-            msg: 'Token not found'
-        })
-    }
-    // TODO: Save in DataBase
-    res.json({
-        ok: true,
-        googleUser
-    });
+
 
 };
 
